2c36ece08027e8b09ab3a01a5b196bb7c21c212d
Nishant Kohli <nishkohli96@gmail.com>
2022-08-23 17:59:51 +0530

    rename stuff

--- src/components/Card/helpers.ts
+++ src/components/Card/helpers.ts
@@ -1,7 +1,7 @@
 import { mapCardIndexToSuit, mapCardIndexToValue } from './card-mapping';
 import { PlayingCardProps } from 'types';
 
-export const generateCardsArray = (cards: number[]) => {
+export function generateCardsArray(cards: number[]) {
 	const cardsArr: PlayingCardProps[] = [];
 	cards.map((card) => {
 		const suitIndex = Math.floor(card / 13);
@@ -20,4 +20,4 @@ export const generateCardsArray = (cards: number[]) => {
 		return cardsArr;
 	});
 	return cardsArr;
-};
+}

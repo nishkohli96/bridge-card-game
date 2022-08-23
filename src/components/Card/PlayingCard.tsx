c9a43d2f39e6b1a2e70e9a21d750a6d285736cbc
Nishant Kohli <nishkohli96@gmail.com>
2022-08-23 07:46:45 +0530

    card visibility

--- src/components/Card/PlayingCard.tsx
+++ src/components/Card/PlayingCard.tsx
@@ -1,23 +1,27 @@
 import { FC } from 'react';
 import { CardValues, CardSuits } from './CardTypes';
 import { Card, styled } from '@mui/material';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import CardSuitIcon from './CardSuitIcon';
 
 export interface PlayingCardProps {
 	faceValue: CardValues;
 	suit: CardSuits;
+	isVisible?: boolean;
 }
 
 export const PlayingCard: FC<PlayingCardProps> = (props) => {
+	const { isVisible, faceValue, suit } = props;
 	const CardContainer = styled(Card)({
 		width: 40,
 		height: 60,
 		padding: 5,
+		background: isVisible ? 'white' : '#edb4c1',
 	});
 
 	const CardValueDiv = styled('span')({
 		color:
-			props.suit === CardSuits.DIAMONDS || props.suit === CardSuits.HEARTS
+			suit === CardSuits.DIAMONDS || suit === CardSuits.HEARTS
 				? 'red'
 				: 'black',
 	});
@@ -29,10 +33,18 @@ export const PlayingCard: FC<PlayingCardProps> = (props) => {
 
 	return (
 		<CardContainer>
-			<CardValueDiv>{props.faceValue}</CardValueDiv>
-			<CardValueIcon>
-				<CardSuitIcon suit={props.suit} />
-			</CardValueIcon>
+			{isVisible ? (
+				<>
+					<CardValueDiv>{faceValue}</CardValueDiv>
+					<CardValueIcon>
+						<CardSuitIcon suit={suit} />
+					</CardValueIcon>
+				</>
+			) : (
+				<CardValueIcon>
+					<QuestionMarkIcon />
+				</CardValueIcon>
+			)}
 		</CardContainer>
 	);
 };

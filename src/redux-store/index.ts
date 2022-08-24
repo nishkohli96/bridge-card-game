ffa464f6294fd672c384fe4e255d6ab982c49325
Nishant Kohli <nishkohli96@gmail.com>
2022-08-24 20:22:09 +0530

    Dev (#5)
    * first commit
    
    * icon wip
    
    * card design wip
    
    * card layout done
    
    * rm routing
    
    * wip
    
    * wip
    
    * minor changes
    
    * starting card deck
    
    * card deck layout basic
    
    * wip
    
    * cleaner ui
    
    * wip
    
    * ui done
    
    * card visibility
    
    * intro dialog almost done
    
    * open btn link in new tab
    
    * helpdialog done
    
    * rename & rearrange stuff
    
    * shuffle cards array
    
    * some changes
    
    * reset game dialog done
    
    * card mapping wip
    
    * card mapping done
    
    * render all cards
    
    * card assign clockwise direction
    
    * memoization wip
    
    * memoization done
    
    * minor updates
    
    * add aos
    
    * wip
    
    * card selection ??
    
    * gameplay Selector
    
    * rename stuff
    
    * score mapping fn
    
    * wip
    
    * scorecard
    
    * rm aos
    
    * alignment issues
    
    * visibility mapping
    
    * wip
    
    * layout change
    
    * minor changes
    
    * add class for player turn
    
    * minor change
    
    * showAllCards
    
    * drawer wip
    
    * drawer layout done
    
    * wip
    
    * card decks horizontal
    
    * move visibilityAll
    
    * wip
    
    * cards distribution fn
    
    * score calculation
    
    * Update README.md
    
    * Rename Cards.ts to cards.ts

--- src/redux-store/index.ts
+++ src/redux-store/index.ts
@@ -2,7 +2,6 @@ export * from './hooks';
 export * from './store';
 export * from './types';
 export * from './selectors';
-export * from './selectors';
 
 export * from './appSetup.slice';
 export * from './gameplay.slice';
